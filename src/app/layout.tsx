268b1d2d9c162f41d084bf06e92cc432de4da245
Joche3023 <j.zumbado.1@estudiantec.cr>
2026-05-09 12:33:47 -0600

    fix(layout): fix de lint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>
+      <body suppressHydrationWarning>
         <MsalProviderWrapper>{children}</MsalProviderWrapper>
       </body>
     </html>
